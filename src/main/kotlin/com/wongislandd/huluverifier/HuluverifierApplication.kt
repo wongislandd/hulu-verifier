757a0fba239f069c8aa1d6acf4f8e3685f911fed
Christopher Wong <46093907+wongislandd@users.noreply.github.com>
2023-07-06 20:04:15 -0400

    Setting up spring files

---
+++ src/main/kotlin/com/wongislandd/huluverifier/HuluverifierApplication.kt
@@ -0,0 +1,11 @@
+package com.wongislandd.huluverifier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HuluverifierApplication
+
+fun main(args: Array<String>) {
+	runApplication<HuluverifierApplication>(*args)
+}
---
+++ src/test/kotlin/com/wongislandd/huluverifier/HuluverifierApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wongislandd.huluverifier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HuluverifierApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
