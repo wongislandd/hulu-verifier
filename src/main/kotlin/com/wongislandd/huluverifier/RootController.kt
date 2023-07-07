85ea39990a5b58e2cb016550342a7870ef17d6bc
Christopher Wong <46093907+wongislandd@users.noreply.github.com>
2023-07-06 23:45:48 -0400

    changed time difference to seconds

--- src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
+++ src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
@@ -1,10 +1,10 @@
 package com.wongislandd.huluverifier
 
-import org.apache.catalina.manager.StatusTransformer.formatTime
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
 private const val DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss"
 private const val SUPER_SECRET_AUTH_CODE = "chrisiscool123"
@@ -28,15 +28,18 @@ class RootController {
     @PostMapping("/")
     fun newCode(@RequestParam auth: String, @RequestBody newCode: String): String {
         return if (auth == SUPER_SECRET_AUTH_CODE) {
-            latestCode = VerificationCode(newCode, getCurrentTime())
+            latestCode = VerificationCode(newCode, System.currentTimeMillis())
             "Successfully updated code to $latestCode"
         } else {
             "Invalid authentication!"
         }
     }
 
-    private fun getCurrentTime(): String {
-        return formatTime(LocalDateTime.now())
+    private fun getTimeDifference(previousTime: Long): String {
+        val now = System.currentTimeMillis()
+        val difference = now - previousTime
+        val secondsDifference = TimeUnit.MILLISECONDS.toSeconds(difference)
+        return "$secondsDifference seconds"
     }
 
     private fun formatTime(time: LocalDateTime): String {
@@ -48,7 +51,7 @@ class RootController {
                 "<div> I built this so I can automate providing you the verification code to my Hulu. This whole workflow checks my email for the Hulu login verification code <i>every minute</i>, parses the email for the code, then updates here!</div>" +
                 "<br>" +
                 "<div>The verification code you are looking for is: <b>${verificationCode.code}</b></div>" +
-                "<div>This was last updated at ${verificationCode.lastUpdated}</div>" +
+                "<div>This was last updated ${getTimeDifference(verificationCode.lastUpdated)} ago.</div>" +
                 "<div>If this isn't the valid code, please wait up to a minute for the thing to do the thingy.</div>" +
                 "<br>" +
                 "<div>If you have any suggestions for this flow, let me know!</div>"
--- src/main/kotlin/com/wongislandd/huluverifier/VerificationCode.kt
+++ src/main/kotlin/com/wongislandd/huluverifier/VerificationCode.kt
@@ -1,6 +1,5 @@
 package com.wongislandd.huluverifier
 
 private const val DEFAULT_CODE = "default"
-private const val DEFAULT_TIME_UPDATED = "never"
 
-data class VerificationCode(val code: String =  DEFAULT_CODE, val lastUpdated: String = DEFAULT_TIME_UPDATED)
+data class VerificationCode(val code: String =  DEFAULT_CODE, val lastUpdated: Long = System.currentTimeMillis())
