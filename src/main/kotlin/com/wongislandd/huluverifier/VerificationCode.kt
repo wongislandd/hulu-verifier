7b278144f3b5ae51a1806fe8896d79868388c039
Christopher Wong <46093907+wongislandd@users.noreply.github.com>
2023-07-06 22:43:22 -0400

    updating gen

--- src/main/kotlin/com/wongislandd/huluverifier/VerificationCode.kt
+++ src/main/kotlin/com/wongislandd/huluverifier/VerificationCode.kt
@@ -1,6 +1,6 @@
 package com.wongislandd.huluverifier
 
 private const val DEFAULT_CODE = "default"
-private const val DEFAULT_TIME_SET = "never"
+private const val DEFAULT_TIME_UPDATED = "never"
 
-data class VerificationCode(private val code: String =  DEFAULT_CODE, private val timeSet: String = DEFAULT_TIME_SET)
+data class VerificationCode(private val code: String =  DEFAULT_CODE, private val lastUpdated: String = DEFAULT_TIME_UPDATED)
