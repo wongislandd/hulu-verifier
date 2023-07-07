c44813eea59667db9738b6e4fa46f34ed1e5d484
Christopher Wong <46093907+wongislandd@users.noreply.github.com>
2023-07-07 00:55:14 -0400

    making it green

--- src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
+++ src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
@@ -42,18 +42,26 @@ class RootController {
         return "$secondsDifference seconds"
     }
 
-    private fun formatTime(time: LocalDateTime): String {
-        return timeFormatter.format(time)
-    }
-
     private fun getFormattedMessage(verificationCode: VerificationCode): String {
-        return "<div><h1> Chris's Hulu verification center: </h1></div>" +
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "body {background-color: #1ce783;}\n" +
+                "h1   {color: #151515;}\n" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>" +
+                "<div><h1> Chris's Hulu verification center: </h1></div>" +
                 "<div> I built this so I can automate providing you the verification code to my Hulu. This whole workflow checks my email for the Hulu login verification code <i>every minute</i>, parses the email for the code, then updates here!</div>" +
                 "<br>" +
-                "<div>The verification code you are looking for is: <b>${verificationCode.code}</b></div>" +
+                "<div>The verification code you are looking for is:</div>" +
+                "<div> <h2>${verificationCode.code}</h2></div>" +
                 "<div>This was last updated ${getTimeDifference(verificationCode.lastUpdated)} ago.</div>" +
                 "<br>" +
                 "<div>If this isn't the valid code, please wait up to a minute for the thing to do the thingy.</div>" +
-                "<div>If you have any suggestions for this flow, let me know!</div>"
+                "<div>If you have any suggestions for this flow, let me know!</div>" +
+                "</body>" +
+                "</html>"
     }
 }
