83df059d8caf38ffe4bf0ed28db0dc18b0eb0bd4
Christopher Wong <46093907+wongislandd@users.noreply.github.com>
2023-07-06 23:31:53 -0400

    updating to be pretty

--- src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
+++ src/main/kotlin/com/wongislandd/huluverifier/RootController.kt
@@ -1,6 +1,7 @@
 package com.wongislandd.huluverifier
 
 import org.apache.catalina.manager.StatusTransformer.formatTime
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -14,9 +15,9 @@ class RootController {
     private val timeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)
     private var latestCode = VerificationCode()
 
-    @RequestMapping("/")
+    @RequestMapping("/", produces = arrayOf(MediaType.TEXT_HTML_VALUE))
     fun getCodePretty(): String {
-        return "Verification code: ${latestCode.code} ------- Last updated: ${latestCode.lastUpdated}"
+        return getFormattedMessage(latestCode)
     }
 
     @RequestMapping("/code")
@@ -40,5 +41,16 @@ class RootController {
 
     private fun formatTime(time: LocalDateTime): String {
         return timeFormatter.format(time)
+    }
+
+    private fun getFormattedMessage(verificationCode: VerificationCode): String {
+        return "<div><h1> Chris's Hulu verification center: </h1></div>" +
+                "<div> I built this so I can automate providing you the verification code to my Hulu. This whole workflow checks my email for the Hulu login verification code <i>every minute</i>, parses the email for the code, then updates here!</div>" +
+                "<br>" +
+                "<div>The verification code you are looking for is: <b>${verificationCode.code}</b></div>" +
+                "<div>This was last updated at ${verificationCode.lastUpdated}</div>" +
+                "<div>If this isn't the valid code, please wait up to a minute for the thing to do the thingy.</div>" +
+                "<br>" +
+                "<div>If you have any suggestions for this flow, let me know!</div>"
     }
 }
