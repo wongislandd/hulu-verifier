239e721db4acac0f329642c48b9c62e924bfaf86
Christopher Wong <cwong51799@gmail.com>
2024-12-20 13:24:27 -0500

    Update findcode.py

--- find-code-func/findcode.py
+++ find-code-func/findcode.py
@@ -1,20 +1,21 @@
 import logging
-
+import json
 import azure.functions as func
 import re
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    fullBody = req.get_body().decode()
-    logging.info(fullBody)
-    maybeVerificationCodeIdentified = re.findall(r'>\d{6}<', fullBody)
-    if maybeVerificationCodeIdentified:
-        verificationCodeParsed = re.findall(r'\d{6}', maybeVerificationCodeIdentified[0])[0]
-        logging.info(f'Verification code found: {verificationCodeParsed}')
-        return func.HttpResponse(verificationCodeParsed)
+    body_str = req.get_body().decode()
+    body = json.loads(body_str)
+    sender = body["SenderName"].replace("\"", "")
+    snippet = body["Snippet"]
+    maybe_verification_code_identified = re.findall(r'\b\d{6}\b', snippet)
+    if maybe_verification_code_identified:
+        verification_code_parsed = re.findall(r'\d{6}', maybe_verification_code_identified[0])[0]
+        logging.info(f'Verification code from {sender} found: {verification_code_parsed}')
+        return func.HttpResponse(f"{sender} code: {verification_code_parsed}")
     else:
         return func.HttpResponse(
-             "No verification code found. Body: " + str(fullBody),
+             body_str,
              status_code=404
         )
